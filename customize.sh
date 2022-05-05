222ce09d04ed1f4bba00e81b2590f8ac0e4632eb
max44 <76995969+giving-sesame@users.noreply.github.com>
2022-05-05 11:19:15 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,35 +4,34 @@ ui_print   "* |_   _|__  /  _ \  / \|_   _|/ \    *"
 ui_print   "*   | |   / /| | | |/ _ \ | | / _ \   *"  
 ui_print   "*   | |  / /_| |_| / ___ \| |/ ___ \  *"
 ui_print   "*   |_| /____|____/_/   \_\_/_/   \_\ *"
+ui_print   "*                                     *"
 ui_print   "*     ~Systemless TZdata Updater~     *"
-ui_print   "*          ~By: Ryaniskira~           *"
 ui_print   "*                                     *"
 ui_print   "***************************************"
-
-ui_print " "
-ui_print "  Let's detect what is the version of your Android"
-ui_print " "
 
 # Variables
 DEVFND=0
 SDK_VER=1
-SDK_VER_MAX=31
+SDK_VER_MAX=33
 # SDK check
   if [ $API -ge $SDK_VER ] && [ $API -lt $SDK_VER_MAX ]; then
-    ui_print "SDK$API detected. It is supported."
+    ui_print "SDK $API detected. It is supported."
     DEVFND=1
   fi
 # Abort if no match
 if [ $DEVFND == 0 ]; then
-  abort "Android is newer than Android 11 or modified build.prop! Aborting."
+  abort "Android SDK $API is not supported. Aborting..."
 fi
 
-ui_print "   Let's install tzdata for SDK$API"
+ui_print " "
+ui_print "Installing tzdata..."
 if [ $API -ge 29 ]; then 
-	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/tzdataN $MODPATH/system/usr/share/zoneinfo/tzdata
-	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/tz_versionN $MODPATH/system/usr/share/zoneinfo/tz_version
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/10/distro_version $MODPATH/system/usr/share/zoneinfo/distro_version
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/10/tzdata $MODPATH/system/usr/share/zoneinfo/tzdata
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/10/tzlookup.xml $MODPATH/system/usr/share/zoneinfo/tzlookup.xml
 else
-	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/tzdataO $MODPATH/system/usr/share/zoneinfo/tzdata
-	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/tz_versionO $MODPATH/system/usr/share/zoneinfo/tz_versionO
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/9/distro_version $MODPATH/system/usr/share/zoneinfo/distro_version
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/9/tzdata $MODPATH/system/usr/share/zoneinfo/tzdata
+	cp -afr $MODPATH/system/usr/share/zoneinfo/SOURCES/9/tzlookup.xml $MODPATH/system/usr/share/zoneinfo/tzlookup.xml
 fi
 rm -rf $MODPATH/system/usr/share/zoneinfo/SOURCES/
